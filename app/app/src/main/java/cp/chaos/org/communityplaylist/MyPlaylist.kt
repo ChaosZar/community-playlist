13004642ca0bf3c4a6e1fd75a53029fab2ee3411
Anika Bracht <abracht@fb3.uni-bremen.de>
2018-02-03 13:47:27 +0100

    Remove mail icon.

--- app/app/src/main/java/cp/chaos/org/communityplaylist/MyPlaylist.kt
+++ app/app/src/main/java/cp/chaos/org/communityplaylist/MyPlaylist.kt
@@ -14,11 +14,6 @@ class MyPlaylist : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_playlist)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
