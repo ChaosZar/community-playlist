96ad1c3988ea093ec026b0f27a05496ab5164ba4
Anika Bracht <abracht@fb3.uni-bremen.de>
2018-02-03 16:02:04 +0100

    Adds GlobalPlayList Activity and Button function.

--- CommunityPlaylistApp/app/src/main/java/com/communityplaylistapp/MainActivity.java
+++ CommunityPlaylistApp/app/src/main/java/com/communityplaylistapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.communityplaylistapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openGlobalPlaylist(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, GlobalPlaylist.class);
+        startActivity(intent);
     }
 }
