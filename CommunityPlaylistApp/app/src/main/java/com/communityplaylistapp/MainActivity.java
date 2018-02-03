b0d94cc8ac32dc1655044837896eeebb2270375c
Anika Bracht <abracht@fb3.uni-bremen.de>
2018-02-03 14:12:45 +0100

    Java app base

---
+++ CommunityPlaylistApp/app/src/main/java/com/communityplaylistapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.communityplaylistapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
