b5f9d450cfefe67f03d8c6d8c9d28e0bf1077de6
Anika Bracht <abracht@fb3.uni-bremen.de>
2018-02-03 17:51:27 +0100

    Adds text field for entering name and validation.

--- app/app/src/main/java/cp/chaos/org/communityplaylist/LoginActivity.java
+++ app/app/src/main/java/cp/chaos/org/communityplaylist/LoginActivity.java
@@ -3,6 +3,7 @@ package cp.chaos.org.communityplaylist;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +14,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginClick(View view) {
+        EditText userName = findViewById(R.id.userName);
+        if (userName.getText().toString().trim().isEmpty()) {
+            userName.setError("Bitte gib deinen Namen an");
+            return;
+        }
         finish();
     }
 }
