0a6b2d7191f75562d35a1a197bc2b73936d83e41
justus- <jbuecker@gmx.net>
2018-02-03 17:28:28 +0100

    Basic login activity switch

--- app/app/src/main/java/cp/chaos/org/communityplaylist/OverviewActivity.java
+++ app/app/src/main/java/cp/chaos/org/communityplaylist/OverviewActivity.java
@@ -1,18 +1,19 @@
 package cp.chaos.org.communityplaylist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class OverviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_overview);
+        startActivity(new Intent(this, LoginActivity.class));
     }
 
-    public void loginClick(View view) {
-        finish();
+    public void openSearchClick(View view) {
     }
 }
