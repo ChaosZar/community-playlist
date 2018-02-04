f631ea5dde8a0c50ab2d13e8eca406913a295aa1
Dennis Kieselhorst <mail@dekies.de>
2018-02-04 12:41:39 +0100

    it's now possible to stream a song

--- backend/src/main/java/org/chaos/cp/connector/SpotifyMusicStorage.java
+++ backend/src/main/java/org/chaos/cp/connector/SpotifyMusicStorage.java
@@ -2,6 +2,7 @@ package org.chaos.cp.connector;
 
 import com.wrapper.spotify.Api;
 import com.wrapper.spotify.exceptions.WebApiException;
+import com.wrapper.spotify.methods.ArtistSearchRequest;
 import com.wrapper.spotify.methods.TrackSearchRequest;
 import com.wrapper.spotify.models.Page;
 import com.wrapper.spotify.models.Track;
@@ -14,13 +15,12 @@ import java.util.stream.Collectors;
 
 @Service
 public class SpotifyMusicStorage implements MusicStorage {
+    // Create an API instance. The default instance connects to https://api.spotify.com/.
+    private static final Api API = Api.DEFAULT_API;
 
     @Override
     public Collection<Song> findSongsByTitle(String title) throws IOException {
-        // Create an API instance. The default instance connects to https://api.spotify.com/.
-        Api api = Api.DEFAULT_API;
-
-        TrackSearchRequest searchRequest = api.searchTracks(title).build();
+        TrackSearchRequest searchRequest = API.searchTracks(title).build();
 
         try {
             Page<Track> trackPage = searchRequest.get();
@@ -29,6 +29,23 @@ public class SpotifyMusicStorage implements MusicStorage {
         } catch (WebApiException e) {
             throw new IOException(e);
         }
+    }
+
+    @Override
+    public Collection<Song> findSongsByArtist(String artistName) throws IOException {
+        ArtistSearchRequest searchRequest = API.searchArtists(artistName).build();
+
+        try {
+            Page<com.wrapper.spotify.models.Artist> trackPage = searchRequest.get();
+            throw new UnsupportedOperationException("not implemented yet");
+        } catch (WebApiException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public Song getSong(Long id) throws IOException {
+        throw new UnsupportedOperationException("not implemented yet");
     }
 
     @Override
--- backend/src/main/java/org/chaos/cp/rest/SongService.java
+++ backend/src/main/java/org/chaos/cp/rest/SongService.java
@@ -5,18 +5,20 @@ import org.chaos.cp.entity.Song;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/song")
@@ -29,7 +31,7 @@ public class SongService {
     @RequestMapping(path = "/{name}", method = RequestMethod.GET)
     public @ResponseBody
     List<Song> findSongsByName(@PathVariable(value = "name") String name) {
-        return musicStorages.stream().filter(MusicStorage::isEnabled)
+        return getEnabledMusicStorages()
                 .flatMap(musicStorage -> {
                     Collection<Song> songs;
                     try {
@@ -42,10 +44,42 @@ public class SongService {
                 }).collect(Collectors.toList());
     }
 
+    private Stream<MusicStorage> getEnabledMusicStorages() {
+        return musicStorages.stream().filter(MusicStorage::isEnabled);
+    }
+
     @RequestMapping(path = "/artist/{name}", method = RequestMethod.GET)
     public @ResponseBody
     List<Song> findSongsByArtist(@PathVariable(value = "name") String name) {
-        throw new NotImplementedException();
+        return getEnabledMusicStorages().flatMap(musicStorage -> {
+            Collection<Song> songs;
+            try {
+                songs = musicStorage.findSongsByArtist(name);
+            } catch (IOException e) {
+                LOG.error("Error while finding songs", e);
+                songs = Collections.emptyList();
+            }
+            return songs.stream();
+        }).collect(Collectors.toList());
     }
 
+    @RequestMapping(path = "/stream/{songId}", method = RequestMethod.GET)
+    @ResponseBody
+    public Resource getSong(@PathVariable(value = "songId") Long songId) {
+        Collection<MusicStorage> enabledMusicStorages =
+                getEnabledMusicStorages().collect(Collectors.toList());
+        Song song = null;
+        for (MusicStorage musicStorage : enabledMusicStorages) {
+            try {
+                song = musicStorage.getSong(songId);
+            } catch (IOException e) {
+                LOG.error("Error while getting song {}", songId, e);
+            }
+            // TODO check for type and only return FileSystemResource if it's local song
+            if (song != null) {
+                return new FileSystemResource(song.getRef());
+            }
+        }
+        throw new IllegalArgumentException("invalid song " + songId);
+    }
 }
