2bc290a114a826027183b96db19a401aa70eb0e4
justus- <jbuecker@gmx.net>
2018-02-04 14:53:51 +0100

    Return value from login activity

---
+++ app/app/src/main/java/cp/chaos/org/communityplaylist/json/SearchResults.java
@@ -0,0 +1,20 @@
+package cp.chaos.org.communityplaylist.json;
+
+import java.util.List;
+
+/**
+ * @author jbuecker
+ * @since 4.6.
+ */
+
+public class SearchResults {
+    private List<Song> songs;
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+}
